50c1f6b88eeadafac20f8ba36680664395c200a0
missymaloney <missymaloney1@gmail.com>
2025-05-14 12:36:59 -0400

    removed api stuff

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 
-var corsOptions = {
-  origin: 'http://localhost:3000',
-};
+// var corsOptions = {
+//   origin: 'http://localhost:3000',
+// };
 
 const app = express();
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -15,19 +15,19 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require('./app/models');
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to the database!');
-  })
-  .catch((err) => {
-    console.log('Cannot connect to the database!', err);
-    process.exit();
-  });
+//const db = require('./app/models');
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Connected to the database!');
+//   })
+//   .catch((err) => {
+//     console.log('Cannot connect to the database!', err);
+//     process.exit();
+//   });
 
 // simple route
 app.get('/', (req, res) => {
