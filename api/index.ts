f6f2b6881e801899acdf96a5f0ba92b5d3bf47ec
missymaloney <missymaloney1@gmail.com>
2025-05-14 11:07:41 -0400

    added missy

--- api/index.ts
+++ api/index.ts
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Express on Vercel');
+  res.send('Express on Vercel Missy');
 });
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => console.log('Server ready on port 5001.'));
+app.listen(PORT, () => console.log('Server ready on port 3000.'));
 
 module.exports = app;
