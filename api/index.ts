9b738e6da0bdf867cab808f920d7e8608cc4319a
missymaloney <missymaloney1@gmail.com>
2025-05-14 12:51:59 -0400

    added maloney

--- api/index.ts
+++ api/index.ts
@@ -15,23 +15,9 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-//const db = require('./app/models');
-// db.mongoose
-//   .connect(db.url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('Connected to the database!');
-//   })
-//   .catch((err) => {
-//     console.log('Cannot connect to the database!', err);
-//     process.exit();
-//   });
-
 // simple route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Missy  application.' });
+  res.json({ message: 'Welcome to Missy Maloney application.' });
 });
 
 require('./routes/user.routes')(app);
