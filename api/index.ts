50ae47f7eb918e820f24679c6368e4fe9b9dfb86
missymaloney <missymaloney1@gmail.com>
2025-05-15 19:54:48 -0400

    added cors

--- api/index.ts
+++ api/index.ts
@@ -3,15 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
+app.use(cors());
 app.use(express.json());
 
 const usersRouter = require('./routes/user.routes');
 app.use('/syc/users', usersRouter);
 
-app.listen(3000, () => console.log('Server Started'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
